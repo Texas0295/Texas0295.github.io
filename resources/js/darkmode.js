c9db3e098d0f8baae642792a68ba8be777536699
Texas0295 <Texas0295@outlook.com>
2024-09-28 14:07:48 +0800

    DarkMode

---
+++ resources/js/darkmode.js
@@ -0,0 +1,32 @@
+let darkmode = localStorage.getItem("darkmode");
+const darkModeToggle = document.getElementById("theme-switch");
+
+function detectDarkMode() {
+    if (darkmode === null) {
+        let userDefaultIsDarkMode = window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches;
+        darkmode = userDefaultIsDarkMode ? "active" : "disabled";
+        localStorage.setItem("darkmode", darkmode);
+    }
+}
+
+function enableDarkMode() {
+    document.body.classList.add("dark");
+    localStorage.setItem("darkmode", "active");
+}
+
+function disableDarkMode() {
+    document.body.classList.remove("dark");
+    localStorage.setItem("darkmode", "disabled");
+}
+
+detectDarkMode();
+if (darkmode === "active") {
+    enableDarkMode();
+} else {
+    disableDarkMode();
+}
+
+darkModeToggle.addEventListener("click", () => {
+    darkmode = localStorage.getItem("darkmode");
+    darkmode !== "active" ? enableDarkMode() : disableDarkMode();
+});
